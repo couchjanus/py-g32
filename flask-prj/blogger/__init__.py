487983c75e8253ffd8d797927ffcffd41a3d5fce
couchjanus <couchjanus@gmail.com>
2023-09-05 21:40:41 +0300

    added unit 17

--- flask-prj/blogger/__init__.py
+++ flask-prj/blogger/__init__.py
@@ -1,37 +1,38 @@
-from flask import Flask
+# __init__.py
 import os
-# app = Flask(__name__)
 
-# @app.route('/hello')
-# def hello():
-#     return '<h1>Hello Flask!</h1>'
+from flask import Flask
 
+def create_app(test_config=None):
+    # create and configure the app
 
-def create_app():
     app = Flask(__name__, instance_relative_config=True)
-
-    @app.route('/hello')
-    def hello():
-        return '<h1>Hello Flask!</h1>'
     
     app.config.from_mapping(
-        SECRET_KEY='Bla bla bla',
-        DATABASE=os.path.join(app.instance_path, 'blogger.db') 
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'blogger.db'),
     )
-    
+
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    from . import db
     
-    from .import db
     db.init_app(app)
-    
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
-    
-    from . import auth
-    app.register_blueprint(auth.bp)
-    
+
     return app
--- flask-prj/blogger/auth.py
+++ flask-prj/blogger/auth.py
@@ -1,9 +1,36 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+# auth.py
+
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from blogger.db import get_db
 
-from werkzeug.security import generate_password_hash
-
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -12,28 +39,54 @@ def register():
         password = request.form['password']
         db = get_db()
         error = None
+
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
+
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)", 
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password)),
                 )
                 db.commit()
             except db.IntegrityError:
                 error = f"User {username} is already registered."
             else:
-                return redirect(url_for('auth.login'))
-    return render_template('auth/register.html')
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template('auth/register.html', title="Sign On")
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
-    return render_template('auth/login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html', title="Sign In")
 
 @bp.route('/logout')
 def logout():
-    return "Sign Out"
+    session.clear()
+    return redirect(url_for('index'))
